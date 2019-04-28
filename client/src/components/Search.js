d3afe9283389bf4173dca90604e54010f65a5146
Ramin Rabani <rrabani2@gmail.com>
2019-04-28 13:41:23 -0700

    Revise search to console.log input (step 4)

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -4,15 +4,27 @@ class Search extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-
+      query: ''
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange (e) {
+    let query = e.target.value;
+    this.setState({ query } /*, () => console.log(this.state) */ )
+  }
+
+  handleSubmit (e) {
+    e.preventDefault();
+    console.log(this.state);
   }
 
   render() {
     return (
       <div>
-        <form action="" >
-          <input type="text" />
+        <form action="" onSubmit={this.handleSubmit} >
+          <input type="text" onChange={this.handleChange}/>
           <button>Add Pokemon</button>
         </form>
       </div>
